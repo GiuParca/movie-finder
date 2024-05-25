8edea9baa0b0477958a6cd129e36a1ce9929a553
Giulia Parca <124640350+GiuParca@users.noreply.github.com>
2024-05-25 23:15:19 +0100

    fetching The Movie DB API and creating cards dynamically

---
+++ script.js
@@ -0,0 +1,48 @@
+const api = 'https://api.themovielab.org/3/discover/movie?sort_by=popularity.desc&api_key=062c256e6dcceb85e99718b5574bfbaf&page=1';
+const img_src = 'https://image.tmdb.org/t/p/w1280';
+const search_api = 'https://api.themoviedb.org/3/search/movie?&api_key=062c256e6dcceb85e99718b5574bfbaf&query=';
+
+const main = document.getElementById('section');
+const form = document.getElementById('form');
+const search = document.getElementById('query');
+
+searchedmovies(api);
+
+function searchedmovies(url) {
+    fetch(url)
+        .then(res => res.json())
+        .then(function (data) {
+            data.results.forEach(element => {
+                const div_card = document.createElement('div');
+                const div_row = document.createElement('div');
+                const div_column = document.createElement('div');
+                const image = document.createElement('img');
+                const title = document.createElement('h3');
+                const center = document.createElement('center');
+
+                title.innerHTML = `${element.title}`;
+                image.src = img_src + element.poster_path;
+
+                center.appendChild(image);
+                div_card.appendChild(center);
+                div_card.appendChild(title);
+                div_column.appendChild(div_card);
+                div_row.appendChild(div_column);
+
+                main.appendChild(div_row);
+            });
+        });
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = '';
+
+    const searchItem = search.value;
+    if (searchItem) {
+        searchedmovies(search_api + searchItem);
+        search.value = " ";
+    }
+}
+
+)
